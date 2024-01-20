6942104ae450b3a7c88241388546d53fc524d687
Abu <abujakaria316@gmail.com>
2024-01-21 03:10:32 +0600

    four variant button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,12 @@ function App() {
   return (
     <>
       <button className="btn">Hover Me</button>
+      <div className="devider"></div>
+      <button className="btn-2">Hover Me 2</button>
+      <div className="devider"></div>
+      <button className="btn-3">Hover Me 3</button>
+      <div className="devider"></div>
+      <button className="btn-4">Hover Me 4</button>
     </>
   );
 }
